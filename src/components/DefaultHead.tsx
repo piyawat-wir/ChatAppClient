bb8c59bab7723e20625b3438b52ab9170804dd96
piyawat-wir <77828805+piyawat-wir@users.noreply.github.com>
2023-02-28 22:11:31 +0700

    Add DefaultHeader

---
+++ src/components/DefaultHead.tsx
@@ -0,0 +1,10 @@
+import Head from "next/head";
+
+export default function DefaultHead() {
+	return <Head>
+		<title>{'ChatCat >w<'}</title>
+		<meta name="description" content="ChatCat RTC App" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="icon" href="/favicon.ico" />
+	</Head>
+}
