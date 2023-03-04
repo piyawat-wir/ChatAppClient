dc88786cb243c528a4d58e09cb648ec5d9cc34d1
piyawat-wir <77828805+piyawat-wir@users.noreply.github.com>
2023-03-04 16:53:14 +0700

    Add types

---
+++ src/lib/types.ts
@@ -0,0 +1,20 @@
+import { AppCookies, CustomIncomingMessage } from "@/web/sessions"
+import { IncomingMessage } from "http"
+import { GetServerSidePropsContext } from "next"
+import { NextApiRequestCookies } from "next/dist/server/api-utils"
+
+export type UserData = {
+	id: string,
+	name: string,
+	profilePicture: string
+}
+
+export type UserRegisterData = {
+	name: string,
+	roomcode: string,
+	profilePicture: string
+}
+
+export interface AppServerSidePropsContext extends GetServerSidePropsContext {
+	req: CustomIncomingMessage
+}
