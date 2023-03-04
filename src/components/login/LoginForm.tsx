d60f0234db2034755ea272f5585616526018c138
piyawat-wir <77828805+piyawat-wir@users.noreply.github.com>
2023-03-04 16:57:13 +0700

    Add submit handler

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,30 +1,54 @@
-import { ChangeEvent, useEffect, useState } from "react"
+import { requestSession } from "@/lib/api/sessions";
+import { createUser } from "@/lib/api/users";
+import { UserRegisterData } from "@/lib/types";
+import { useRouter } from "next/router";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react"
 import styles from './LoginForm.module.css'
 
 export default function LoginForm() {
 
 	const [username, setUsername] = useState('');
-	const [roomCode, setRoomCode] = useState('');
+	const [roomcode, setRoomcode] = useState('');
+	const router = useRouter();
 
-	return (<div style={{display: 'flex', justifyContent: 'center', flexDirection: 'column'}}>
-		<div className={styles.container}>
-			<div><h1>🐈‍⬛ChatCat</h1></div>
-			<div className={styles.fieldBox}>
-				<input type='text'
-					placeholder="Username"
-					onChange={e => setUsername(e.target.value)}
-				/>
-			</div>
-			<div className={styles.fieldBox}>
-				<input type='text'
-					placeholder="Room code"
-					onChange={e => setRoomCode(e.target.value)}
-				/>
-			</div>
-			<div className={styles.buttons}>
-				<button className={styles.createBtn}>Create Room</button>
-				<button className={styles.joinBtn}>Join</button>
-			</div>
+	function verify() {
+	}
+
+	function submitHandler(e: FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+		const userdata: UserRegisterData = {
+			name: username,
+			roomcode: roomcode,
+			profilePicture: ''
+		}
+		
+		createUser(userdata).then(({data})=>{
+			console.log('id:', data.id);
+			router.push(`r/${roomcode}`);
+		});
+	}
+
+	return (
+		<div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
+			<form className={styles.container} onSubmit={submitHandler}>
+				<div><h1>🐈‍⬛ChatCat</h1></div>
+				<div className={styles.fieldBox}>
+					<input type='text' name='name'
+						placeholder="Username"
+						onChange={e => setUsername(e.target.value)}
+					/>
+				</div>
+				<div className={styles.fieldBox}>
+					<input type='text' name='roomcode'
+						placeholder="Room code"
+						onChange={e => setRoomcode(e.target.value)}
+					/>
+				</div>
+				<div className={styles.buttons}>
+					<button className={styles.createBtn}>Create Room</button>
+					<button className={styles.joinBtn}>Join</button>
+				</div>
+			</form>
 		</div>
-	</div>)
+	)
 }
