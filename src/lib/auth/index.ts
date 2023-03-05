7efa91344f96ba99b1cc375b317a19621b730dcf
piyawat-wir <77828805+piyawat-wir@users.noreply.github.com>
2023-03-06 06:09:43 +0700

    Update lib/auth

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -1,8 +1,9 @@
-import { SessionData } from '@/web/sessions'
+import { BackendRequestBody, SessionCookieData, SessionData } from '@/web/sessions'
+import axios, { Axios, AxiosError, AxiosResponse } from 'axios';
 import { UserData } from '../types'
 
 
-export function login(params: SessionData) {
+export function login(params: SessionCookieData) {
 	// TODO: return user data if user exist in chatroom
 
 	const user: UserData = {
@@ -12,4 +13,27 @@ export function login(params: SessionData) {
 	}
 
 	return user;
+}
+const acceptStatus = [200]
+type axiosMethod = Axios['delete'] | Axios['get'] | Axios['post'] |
+	Axios['put'] | Axios['head']
+export interface BackendRequest {
+	path: string
+	axiosMethod: axiosMethod
+	auth: string
+	data?: any
+}
+export async function requestBackend<T>({ path, axiosMethod, auth, data }: BackendRequest) {
+	try {
+		const uri = new URL(path, process.env.BACKEND_URI).href;
+		const body: BackendRequestBody = { auth, data };
+		const res = await axiosMethod<T>(uri, body);
+		if (!acceptStatus.includes(res.status))
+			throw new AxiosError(undefined, String(res.status), res.config, res.request, res);
+		return res;
+	} catch (err) {
+		const res = (err as AxiosError<T>).response || {} as AxiosResponse<T>;
+		// console.error(res)
+		return res;
+	}
 }
