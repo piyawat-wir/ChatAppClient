978f9f5e680f7f1b0ebda68023b3c74ead35e9b6
piyawat-wir <77828805+piyawat-wir@users.noreply.github.com>
2023-03-06 06:05:55 +0700

    Add lib/api/handler

---
+++ src/lib/api/handler.ts
@@ -0,0 +1,18 @@
+import { NextApiResponse } from "next";
+
+export const badRequest = (res: NextApiResponse) => res.status(400).send('bad request')
+export const noAuth = (res: NextApiResponse) => res.status(401).send('no auth')
+export const notFound = (res: NextApiResponse) => res.status(404).send('not found')
+
+export const ok = (res: NextApiResponse) => res.status(200).send('ok')
+
+const errhandler: Record<number, typeof noAuth> = {
+	400: badRequest,
+	401: noAuth,
+	404: notFound,
+}
+
+export function handleErrorStatus(status: number, res: NextApiResponse) {
+	if (!errhandler[status]) return;
+	return errhandler[status](res);
+}
