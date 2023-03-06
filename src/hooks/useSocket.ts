4e66b7fa70989ca5d7a001b41b0c8fdfe344cd3d
piyawat-wir <77828805+piyawat-wir@users.noreply.github.com>
2023-03-06 17:28:13 +0700

    Add error handling

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -1,23 +1,37 @@
 import { getExecEnv, isClient } from "@/lib";
+import { endSession } from "@/lib/api/sessions";
+import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import { Socket, io } from "socket.io-client";
 
 export default function useSocket(sessionToken?: string) {
 
-	const [socket, setSocket] = useState<Socket>(io('', {autoConnect: false}));
+	const [socket, setSocket] = useState<Socket>(io('', { autoConnect: false }));
+	const router = useRouter()
 
 	useEffect(() => {
 		if (!isClient()) return;
-		
+
 		const token = (socket?.auth as { token?: string })?.token;
 		if (token) return;
-		
+
 		socket.close();
 		const socketOptions = { auth: { token: sessionToken } };
 		const newSocket = io(process.env.BACKEND_URI, socketOptions)
 		setSocket(newSocket);
 
 	}, [sessionToken, socket])
+
+	socket.on('error', (err: Error) => {
+		console.error(err);
+		if (err.message == 'InvalidCredential')
+			endSession().then(() => router.push('/'))
+	})
+	socket.on('connect_error', err => {
+		console.error(err);
+		if (err.message == 'InvalidCredential')
+			endSession().then(() => router.push('/'))
+	})
 
 	return socket;
 }
