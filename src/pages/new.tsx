9356f0a4357c20805ca5a16a30edf23f617534a4
piyawat-wir <77828805+piyawat-wir@users.noreply.github.com>
2023-03-06 17:29:26 +0700

    Add page new.tsx

---
+++ src/pages/new.tsx
@@ -0,0 +1,34 @@
+import styles from '@/styles/Home.module.css'
+import DefaultHead from '@/components/DefaultHead'
+import { AppServerSidePropsContext } from '@/lib/types'
+import { getSessionData } from '@/web/sessions'
+import { GetServerSideProps } from 'next'
+import RegisterForm from '@/components/register/RegisterForm'
+
+export default function Home() {
+	return (
+		<>
+			<DefaultHead />
+			<main className={styles.main}>
+				<div className={styles.sidebar}>
+					<RegisterForm />
+				</div>
+				<div className={styles.body}>
+				</div>
+			</main>
+		</>
+	)
+}
+
+export const getServerSideProps: GetServerSideProps =
+	async ({ req }: AppServerSidePropsContext) => {
+		const session = await getSessionData(req) || null;
+		if (session) return {
+			redirect: {
+				destination: `r/${session.roomcode}`,
+				permanent: false
+			}
+		};
+
+		return { props: { } }
+	}
