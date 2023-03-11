caef2806e011555039b1724b5266820f4624c842
piyawat-wir <77828805+piyawat-wir@users.noreply.github.com>
2023-03-11 23:34:42 +0700

    Update component MsgBox

--- src/components/chat/MsgBox.tsx
+++ src/components/chat/MsgBox.tsx
@@ -13,23 +13,18 @@ type Prop = {
 	righted: boolean
 }
 
-const profmap: Record<string, string> = {
-	'Red Dum': 'reddumb',
-	'Bored_fox': 'boredfox',
-}
-
 export default function MsgBox({ msg, righted }: Prop) {
 
-	const profpic = ProfilePicture[profmap[msg.user]];
+	const profpic = ProfilePicture[msg.profilePicture];
 
 	return <div className={`${styles.msg} ${(righted)?styles.right:''}`}>
-		<Image src={profpic} alt={profmap[msg.user]}
+		<Image src={profpic} alt={msg.profilePicture}
 			width={100} height={100} priority
 		/>
 		<div className={styles.content}>
-			<div className={styles.name}>{msg.user}</div>
+			<div className={styles.name}>{msg.username}</div>
 			<div className={styles.msgcontainer}>
-				<p>{msg.text}</p>
+				<p>{msg.data}</p>
 			</div>
 			<time>{new Date(msg.time).toLocaleTimeString()}</time>
 		</div>
