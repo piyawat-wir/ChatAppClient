6bbf097792140acf66a258a9aaaa7101be5dca53
piyawat-wir <77828805+piyawat-wir@users.noreply.github.com>
2023-03-11 23:35:40 +0700

    Implement send message

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -1,15 +1,41 @@
 import Image from 'next/image'
+import { useRef, useState } from 'react'
+import { Socket } from 'socket.io-client'
 import styles from './chat.module.css'
 
-export default function ChatInput() {
+interface Props {
+	socket: Socket
+}
+
+export default function ChatInput({ socket }: Props) {
+
+	const [message, setMessage] = useState('');
+	const inputBoxRef = useRef<HTMLInputElement | null>(null);
+
+	function sendMessage() {
+		if (!message) return;
+
+		socket.emit('send_message', message);
+		setMessage('');
+		const input = inputBoxRef.current;
+		if (input) input.value = '';
+	}
+
 	return <>
-		<div className={styles.input}>
-			<textarea placeholder='Enter message here...' />
-			<button>
-				Send <Image src={'/img/icon_send.svg'} alt={'send'}
-					width={24} height={24} style={{ filter: 'invert(100%)', marginLeft: '5px' }}
+		<form onSubmit={e => { e.preventDefault(); sendMessage() }}>
+			<div className={styles.input}>
+				<input type='text'
+					className={styles.inputfield}
+					placeholder='Enter message here...'
+					onChange={e => setMessage(e.currentTarget.value)}
+					ref={inputBoxRef}
 				/>
-			</button>
-		</div>
+				<button type='submit' onClick={e => sendMessage()}>
+					Send <Image src={'/img/icon_send.svg'} alt={'send'}
+						width={24} height={24} style={{ filter: 'invert(100%)', marginLeft: '5px' }}
+					/>
+				</button>
+			</div>
+		</form>
 	</>
 }
