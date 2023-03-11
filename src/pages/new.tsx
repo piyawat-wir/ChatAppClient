072ae64427f22167299c4b70a233bc1c7e1a0356
piyawat-wir <77828805+piyawat-wir@users.noreply.github.com>
2023-03-12 00:01:49 +0700

    Refactor getServerSideProps

--- src/pages/new.tsx
+++ src/pages/new.tsx
@@ -1,7 +1,5 @@
 import DefaultHead from '@/components/DefaultHead'
-import { AppServerSidePropsContext } from '@/lib/types'
-import { getSessionData } from '@/web/sessions'
-import { GetServerSideProps } from 'next'
+import { getServerSideProps as getServerSidePropsHandler } from '@/pages'
 import RegisterForm from '@/components/register/RegisterForm'
 import MainLayout from '@/components/layout/Main'
 
@@ -14,17 +12,4 @@ export default function Home() {
 	</>
 }
 
-export const getServerSideProps: GetServerSideProps =
-	async ({ req }: AppServerSidePropsContext) => {
-		const session = await getSessionData(req) || null;
-		if (session?.roomcode) {
-			return {
-				redirect: {
-					destination: `r/${session.roomcode}`,
-					permanent: false
-				}
-			}
-		}
-
-		return { props: {} }
-	}
+export const getServerSideProps = getServerSidePropsHandler
